48fff2d0dafc7d5b10958f368e87a8ebe62b5cbc
Raakesh Madana <madanaraakesh@gmail.com>
2018-03-06 12:11:48 -0600

    Set initial color scheme based on browser's time

--- js/index.js
+++ js/index.js
@@ -14,6 +14,25 @@ function switchColor() {
   }
 }
 
+function setClass(element, cls) {
+  element.classList.add(cls);
+}
+
+function setColorScheme() {
+  var date = new Date();
+  var hours = date.getHours();
+  if (hours > 8 && hours < 18) {
+    setClass(document.getElementById('body'), 'light');
+    setClass(document.getElementById('container'), 'light');
+    setClass(document.getElementById('subhead'), 'light');
+  } else {
+    setClass(document.getElementById('body'), 'dark');
+    setClass(document.getElementById('container'), 'dark');
+    setClass(document.getElementById('subhead'), 'dark');
+  }
+}
+
 window.addEventListener('load', function() {
   document.getElementById('switch').addEventListener('click', switchColor);
+  setColorScheme();
 });
